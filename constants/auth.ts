57dc675ec05ba135aab13c9849a11e98ff30c3fa
mkki <devmkki@gmail.com>
2025-07-31 23:39:11 +0900

    [fix] 웹 배포 URL 변경

--- constants/auth.ts
+++ constants/auth.ts
@@ -4,7 +4,7 @@ export const WEB_VIEW_URL = __DEV__
   ? Platform.OS === 'android'
     ? 'http://10.0.2.2:5173'
     : 'http://localhost:5173'
-  : 'https://status-front-rho.vercel.app';
+  : 'https://app.statoverflow.cloud';
 
 export const MESSAGE_TYPES = {
   AUTH_SUCCESS: 'AUTH_SUCCESS',
