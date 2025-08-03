027f502c4eeeb9542469d8cc71078f7807a45bdc
mkki <devmkki@gmail.com>
2025-08-04 04:56:01 +0900

    [feat] splash screen 추가, 배포 환경 설정

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,21 @@
+import { AnimatedSplashScreen } from '@/components/AnimatedSplashScreen';
 import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
-      <Stack
-        screenOptions={{
-          headerShown: false,
-          contentStyle: { backgroundColor: '#161416' },
-        }}
-      />
+      <AnimatedSplashScreen>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            contentStyle: { backgroundColor: '#161416' },
+          }}
+        />
+      </AnimatedSplashScreen>
     </SafeAreaProvider>
   );
 }
---
+++ components/AnimatedSplashScreen.tsx
@@ -0,0 +1,76 @@
+import { useEvent, useEventListener } from 'expo';
+import * as SplashScreen from 'expo-splash-screen';
+import { VideoSource, VideoView, useVideoPlayer } from 'expo-video';
+import { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const assetId = require('@/assets/images/splash-screen.mp4');
+
+const videoSource: VideoSource = {
+  assetId,
+  metadata: {
+    title: 'Splash Screen',
+  },
+};
+
+export const AnimatedSplashScreen = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [isSplashVideoComplete, setSplashVideoComplete] = useState(false);
+  const [isAppReady, setIsAppReady] = useState(false);
+
+  const player = useVideoPlayer(videoSource, (player) => {
+    player.play();
+  });
+
+  const { isPlaying } = useEvent(player, 'playingChange', {
+    isPlaying: player.playing,
+  });
+
+  useEventListener(player, 'playToEnd', () => {
+    SplashScreen.hideAsync();
+    setSplashVideoComplete(true);
+  });
+
+  useEffect(() => {
+    if (isPlaying) {
+      setIsAppReady(true);
+    }
+  }, [isPlaying]);
+
+  return (
+    <>
+      {!isSplashVideoComplete && (
+        <SafeAreaView style={styles.splashVideoContainer}>
+          <VideoView
+            player={player}
+            nativeControls={false}
+            style={styles.splashVideo}
+          />
+        </SafeAreaView>
+      )}
+      {isAppReady && children}
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  splashVideoContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    zIndex: 100,
+    backgroundColor: '#161416',
+  },
+  splashVideo: {
+    width: 277,
+    height: 382,
+    marginTop: 53,
+    marginHorizontal: 'auto',
+  },
+});
