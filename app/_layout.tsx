5894ede7da22a2438429b617488db89452b52a56
mkki <devmkki@gmail.com>
2025-08-07 16:23:49 +0900

    [fix] Android status bar 아이콘 색상 변경

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,17 @@
 import { AnimatedSplashScreen } from '@/components/AnimatedSplashScreen';
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
+      <StatusBar
+        style="light"
+        backgroundColor="#161416"
+        translucent={false}
+        hidden={false}
+      />
       <AnimatedSplashScreen>
         <Stack
           screenOptions={{
